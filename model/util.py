1dc9f13b96dd2e8a9d1c00be95d799a072caea44
aoxipo <407157175.com>
2023-06-18 12:37:26 +0800

    change show

--- model/util.py
+++ model/util.py
@@ -150,7 +150,7 @@ def _weights_init(m):
         nn.init.normal_(m.weight, 0.0, 0.02)
         nn.init.constant_(m.bias, 0)
 
-def display_progress(cond, real, fake, current_epoch = 0, figsize=(20,15), save = False, save_path = 'D:'):
+def display_progress(cond, real, fake, edge = None, current_epoch = 0, figsize=(20,15), save = False, save_path = 'D:'):
     """
     Save cond, real (original) and generated (fake)
     images in one panel 
@@ -158,25 +158,52 @@ def display_progress(cond, real, fake, current_epoch = 0, figsize=(20,15), save 
     cond = cond.detach().cpu().permute(1, 2, 0).numpy()   
     real = real.detach().cpu().permute(1, 2, 0).numpy()
     fake = fake.detach().cpu().permute(1, 2, 0).numpy()
-    
     images = [cond, real, fake]
     titles = ['input','real','generated']
+
+    if edge is not None:
+        edge = edge.detach().cpu().permute(1, 2, 0).numpy()
+        images.append(edge)
+        titles.append("edge")
+    
+
+    
     print(f'Epoch: {current_epoch}')
-    fig, ax = plt.subplots(1, 3, figsize=figsize)
-    for idx,img in enumerate(images):
-        if idx == 0:
-            imgan = images[0]
+    if edge is None:
+        fig, ax = plt.subplots(1, 3, figsize=figsize)
+        for idx,img in enumerate(images):
+            if idx == 0:
+                imgan = images[0]
+            else:
+                imgan = img
+            imgan = np.array((imgan - np.min(imgan))/(np.max(imgan) - np.min(imgan)) * 255, dtype = np.uint8)
+            imgan = cv2.cvtColor(imgan, cv2.COLOR_RGB2BGR)
+            ax[idx].imshow(imgan)
+            ax[idx].axis("off")
+        for idx, title in enumerate(titles):    
+            ax[idx].set_title('{}'.format(title))
+        if save:
+            f = plt.gcf()  #获取当前图像
+            f.savefig(save_path + '/{}.png'.format(current_epoch))
+            f.clear()  #释放
         else:
-            imgan = img
-        imgan = np.array((imgan - np.min(imgan))/(np.max(imgan) - np.min(imgan)) * 255, dtype = np.uint8)
-        imgan = cv2.cvtColor(imgan, cv2.COLOR_RGB2BGR)
-        ax[idx].imshow(imgan)
-        ax[idx].axis("off")
-    for idx, title in enumerate(titles):    
-        ax[idx].set_title('{}'.format(title))
-    if save:
-        f = plt.gcf()  #获取当前图像
-        f.savefig(save_path + '/{}.png'.format(current_epoch))
-        f.clear()  #释放
+            plt.show()
     else:
-        plt.show()
+        fig, ax = plt.subplots(1, 4, figsize=figsize)
+        for idx,img in enumerate(images):
+            if idx == 0:
+                imgan = images[0]
+            else:
+                imgan = img
+            imgan = np.array((imgan - np.min(imgan))/(np.max(imgan) - np.min(imgan)) * 255, dtype = np.uint8)
+            imgan = cv2.cvtColor(imgan, cv2.COLOR_RGB2BGR)
+            ax[idx].imshow(imgan)
+            ax[idx].axis("off")
+        for idx, title in enumerate(titles):    
+            ax[idx].set_title('{}'.format(title))
+        if save:
+            f = plt.gcf()  #获取当前图像
+            f.savefig(save_path + '/{}.png'.format(current_epoch))
+            f.clear()  #释放
+        else:
+            plt.show()
---
+++ utils/show.py
@@ -0,0 +1,62 @@
+import matplotlib
+matplotlib.use('AGG')
+import matplotlib.pyplot as plt
+from .score import cal_all_score
+import cv2
+import numpy as np
+
+def display_progress(cond, real, fake, edge = None, current_epoch = 0, figsize=(20,15), save = False, save_path = 'D:'):
+    """
+    Save cond, real (original) and generated (fake)
+    images in one panel 
+    """
+    score = cal_all_score(real, fake)
+    cond = cond.detach().cpu().permute(1, 2, 0).numpy()   
+    real = real.detach().cpu().permute(1, 2, 0).numpy()
+    fake = fake.detach().cpu().permute(1, 2, 0).numpy()
+    images = [cond, real, fake]
+    titles = ['input','real','generated']
+
+    if edge is not None:
+        edge = edge.detach().cpu().permute(1, 2, 0).numpy()
+        images.append(edge)
+        titles.append("edge")
+    print(f'Epoch: {current_epoch}')
+    if edge is None:
+        fig, ax = plt.subplots(1, 3, figsize=figsize)
+        for idx,img in enumerate(images):
+            if idx == 0:
+                imgan = images[0]
+            else:
+                imgan = img
+            imgan = np.array((imgan - np.min(imgan))/(np.max(imgan) - np.min(imgan)) * 255, dtype = np.uint8)
+            imgan = cv2.cvtColor(imgan, cv2.COLOR_RGB2BGR)
+            ax[idx].imshow(imgan)
+            ax[idx].axis("off")
+        for idx, title in enumerate(titles):    
+            ax[idx].set_title('{}'.format(title))
+        if save:
+            f = plt.gcf()  #获取当前图像
+            f.savefig(save_path + '/{}_{}.png'.format(current_epoch, score[0].cpu().numpy()))
+            f.clear()  #释放
+        else:
+            plt.show()
+    else:
+        fig, ax = plt.subplots(1, 4, figsize=figsize)
+        for idx,img in enumerate(images):
+            if idx == 0:
+                imgan = images[0]
+            else:
+                imgan = img
+            imgan = np.array((imgan - np.min(imgan))/(np.max(imgan) - np.min(imgan)) * 255, dtype = np.uint8)
+            imgan = cv2.cvtColor(imgan, cv2.COLOR_RGB2BGR)
+            ax[idx].imshow(imgan)
+            ax[idx].axis("off")
+        for idx, title in enumerate(titles):    
+            ax[idx].set_title('{}'.format(title))
+        if save:
+            f = plt.gcf()  #获取当前图像
+            f.savefig(save_path + '/{}_{}.png'.format(current_epoch, score[0].cpu().numpy()))
+            f.clear()  #释放
+        else:
+            plt.show()
